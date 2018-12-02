37a5da0955fcb043cbe0e3931c81728ae245250b
Kyle Peeler <kylepeeler@gmail.com>
2018-12-01 23:57:32 -0500

    update hostname for api requests to not be localhost

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://" + window.location.hostname + ":3000";
 
 function patchData(url, data = {}) {
     //console.log(JSON.stringify(data));
